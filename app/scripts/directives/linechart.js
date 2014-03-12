2abe7f7feae101accf681996a9d19ffd297d0d2b
Santosh Suresh <santoshdeshpande@gmail.com>
2014-03-12 05:54:50 +0530

    Added chart and tabular data

---
+++ app/scripts/directives/linechart.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var app = angular.module('liftApp');
+app.directive('linechart', function () {
+  return {
+    template: '<div></div>',
+    restrict: 'E',
+    link: function postLink(scope, element) {
+      element.text('this is the linechart directive');
+    }
+  };
+});
+
+app.directive('holderFix', function() {
+  return {
+    link: function(scope, element) {
+      Holder.run({images:element[0], nocss:true});
+    }
+  };
+});
