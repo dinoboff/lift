783ef51d42588a28a864e4f5192c1d26a50f8b32
Santosh Suresh <santoshdeshpande@gmail.com>
2014-03-16 22:28:56 +0530

    added meters and bullet chart base

---
+++ app/scripts/controllers/meters.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('liftApp')
+  .controller('MetersCtrl', function ($scope, $http) {
+  });
---
+++ test/client/spec/controllers/meters.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: MetersCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('liftApp'));
+
+  var MetersCtrl,
+    scope,
+    $httpBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    scope = $rootScope.$new();
+    MetersCtrl = $controller('MetersCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+  });
+});
