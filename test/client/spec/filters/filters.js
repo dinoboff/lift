ca632c9f204a573a52f2c82f31b3fded3a6321f1
Santosh Suresh <santoshdeshpande@gmail.com>
2014-03-25 18:09:45 +0530

    Fixed test case

--- test/client/spec/filters/filters.js
+++ test/client/spec/filters/filters.js
@@ -8,12 +8,12 @@ describe('Filter: filters', function () {
   // initialize a new instance of the filter before each test
   var filters;
   beforeEach(inject(function ($filter) {
-    filters = $filter('filters');
+    filters = $filter('toHumanString');
   }));
 
   it('should return the input prefixed with "filters filter:"', function () {
-    var text = 'angularjs';
-    expect(filters(text)).toBe('filters filter: ' + text);
+    var text = 8;
+    expect(filters(text)).toBe('every 8 hours');
   });
 
 });
