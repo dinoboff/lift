c145b70cfb37977714ede60c94d397300fc30a16
Santosh Suresh <santoshdeshpande@gmail.com>
2014-04-12 17:58:26 +0530

    Changed to include the rect rather than circles

--- app/scripts/directives/linechart.js
+++ app/scripts/directives/linechart.js
@@ -44,6 +44,7 @@ app.directive('laDataChart', function() {
       scope.layout = {top: 40, right: 40, bottom: 40, left: 40};
       scope.totalWidth = scope.graph.width +  scope.layout.left + scope.layout.right;
       scope.totalHeight = scope.graph.height + scope.layout.top + scope.layout.bottom;
+
       var x = d3.time.scale().rangeRound([0, scope.graph.width]);
       x.domain(d3.extent(scope.data, function(d) { return d.date; }));
 
@@ -60,6 +61,8 @@ app.directive('laDataChart', function() {
 
       var line1 = d3.svg.line().x(function(d, i) {return x(d.date) }).y(function(d,i) { return y1(d.glucose)}).interpolate('linear');
       var line2 = d3.svg.line().x(function(d, i) {return x(d.date) }).y(function(d,i) { return y2(d.temp)}).interpolate('linear').interpolate('linear');
+
+
 
       scope.glucoseMean = y1(scope.meanGlucose);
       scope.tempMean = y2(scope.meanTemp);
@@ -81,10 +84,12 @@ app.directive('laDataChart', function() {
           .tickSize(0)
           .ticks(d3.time.hours, 6);
 
+      //This needs to be put into the template later.
       var svg = d3.select("svg .chart-container");
       svg.append("g").attr("class", "x axis ").attr('transform', 'translate(0,'+scope.graph.height/2+')').call(xAxis);
       svg.append("g").attr("class", "y axis glucose").call(yAxis1);
       svg.append("g").attr("class", "y axis temp").attr('transform', 'translate('+ scope.graph.width +',0)').call(yAxis2);
+
       scope.line1Path = line1(scope.data);
       scope.line2Path = line2(scope.data);
 
@@ -92,13 +97,14 @@ app.directive('laDataChart', function() {
       scope.tempCircles = [];
       scope.medData = [];
       var medicineCircles = {};
+      //yuck
       angular.forEach(scope.data, function(d) {
         var circle1 = {cx: x(d.date), cy: y1(d.glucose)};
         var circle2 = {cx: x(d.date), cy: y2(d.temp)};
         angular.forEach(d.medicines, function(medicine, index) {
           var s = medicineCircles[medicine.name];
           if (!s) {
-            s = {}
+            s = {};
             s.name = medicine.name;
             s.data = [];
             medicineCircles[medicine.name] = s;
